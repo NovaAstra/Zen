5bafa87c1017846f12784fc49d61b9fc2314537c
August.Gao <August.Gao@mspbots.ai>
2025-05-29 12:22:47 +0800

    main

--- core/linky/src/deque.ts
+++ core/linky/src/deque.ts
@@ -1,6 +1,26 @@
-export type Callback<T, R = boolean> = (value: T, index: number, deque: Deque<T>, node: Node<T>) => R;
+/**
+ * 回调函数类型定义
+ * @template T 元素类型
+ * @template R 返回类型，默认是 boolean
+ * @param value 当前元素值
+ * @param index 当前元素索引
+ * @param deque 当前双端队列实例
+ * @param node 当前节点
+ * @returns 返回值类型 R
+ */
+type Callback<T, R = boolean> = (value: T, index: number, deque: Deque<T>, node: Node<T>) => R;
 
-export class Node<T> {
+/**
+ * 双向链表节点类
+ * @template T 节点存储的元素类型
+ */
+class Node<T> {
+  /**
+ * 创建一个节点
+ * @param value 节点存储的值
+ * @param prev 指向前一个节点的引用
+ * @param next 指向后一个节点的引用
+ */
   public constructor(
     public value: T,
     public prev?: Node<T>,
@@ -8,6 +28,12 @@ export class Node<T> {
   ) { }
 }
 
+/**
+ * 根据输入数组创建一组双向链表节点，返回头尾节点
+ * @template T 元素类型
+ * @param values 要创建节点的元素数组
+ * @returns 返回元组 [头节点, 尾节点]
+ */
 function createNodes<T>(values: T[]): [head: Node<T>, tail: Node<T>] {
   const { head, tail } = values.reduce<{ head: Node<T>; tail: Node<T> }>(
     ({ head, tail }, value, index) => {
@@ -24,6 +50,10 @@ function createNodes<T>(values: T[]): [head: Node<T>, tail: Node<T>] {
   return [head, tail];
 }
 
+/**
+ * 双端队列类，基于双向链表实现
+ * @template T 队列元素类型
+ */
 export class Deque<T> {
   private head?: Node<T>;
 
@@ -31,10 +61,19 @@ export class Deque<T> {
 
   private size: number = 0;
 
+  /**
+   * 创建双端队列实例
+   * @param iterable 可选，初始化的可迭代元素集合
+   */
   constructor(iterable: Iterable<T> = []) {
     this.push(...iterable);
   }
 
+  /**
+  * 从队尾添加元素
+  * @param values 要添加的元素
+  * @returns 添加后队列的长度
+  */
   public push(...values: T[]): number {
     const [head, tail] = createNodes(values);
 
@@ -50,6 +89,10 @@ export class Deque<T> {
     return this.size;
   }
 
+  /**
+  * 从队尾移除元素并返回
+  * @returns 被移除的元素，若队列为空则返回 undefined
+  */
   public pop(): T | undefined {
     if (!this.tail) return undefined;
 
@@ -66,6 +109,10 @@ export class Deque<T> {
     return tail.value;
   }
 
+  /**
+ * 从队首移除元素并返回
+ * @returns 被移除的元素，若队列为空则返回 undefined
+ */
   public shift(): T | undefined {
     if (!this.head) return undefined;
 
@@ -86,6 +133,10 @@ export class Deque<T> {
 
   }
 
+  /**
+  * 反转当前队列的元素顺序，原地修改
+  * @returns 返回当前队列实例
+  */
   public reverse() {
     let node = this.head;
 
@@ -99,6 +150,12 @@ export class Deque<T> {
     return this;
   }
 
+
+  /**
+   * 查找满足条件的元素索引
+   * @param callback 回调函数，用于测试元素
+   * @returns 找到的第一个满足条件的元素索引，找不到返回 -1
+   */
   public findIndex(callback: Callback<T>): number {
     let index = 0;
     let node = this.head;
@@ -111,6 +168,11 @@ export class Deque<T> {
     return -1;
   }
 
+  /**
+  * 查找满足条件的元素值
+  * @param callback 回调函数，用于测试元素
+  * @returns 找到的第一个满足条件的元素，找不到返回 undefined
+  */
   public find(callback: Callback<T>): T | undefined {
     let index = 0;
     let node = this.head;
@@ -123,6 +185,11 @@ export class Deque<T> {
     return undefined;
   }
 
+  /**
+  * 检查所有元素是否都满足条件
+  * @param callback 回调函数，用于测试元素
+  * @returns 如果所有元素都满足条件，返回 true；否则返回 false
+  */
   public every(callback: Callback<T>): boolean {
     let index = 0;
     let node = this.head;
@@ -135,6 +202,11 @@ export class Deque<T> {
     return false;
   }
 
+  /**
+   * 检查是否存在满足条件的元素
+   * @param callback 回调函数，用于测试元素
+   * @returns 存在满足条件的元素返回 true，否则 false
+   */
   public some(callback: Callback<T>): boolean {
     let index = 0;
     let node = this.head;
@@ -147,6 +219,11 @@ export class Deque<T> {
     return false;
   }
 
+  /**
+  * 过滤出满足条件的元素，返回新的双端队列
+  * @param callback 回调函数，用于测试元素
+  * @returns 包含所有满足条件元素的新 Deque 实例
+  */
   public filter(callback: Callback<T>): Deque<T> {
     const deque = new Deque<T>();
 
@@ -163,6 +240,10 @@ export class Deque<T> {
     return deque;
   }
 
+  /**
+   * 遍历所有元素，执行回调函数
+   * @param callback 回调函数，处理元素，无返回值
+   */
   public forEach(callback: Callback<T, void>): void {
     let index = 0;
     let node = this.head;
@@ -174,6 +255,12 @@ export class Deque<T> {
     }
   }
 
+  /**
+  * 映射所有元素，返回映射后的新双端队列
+  * @template N 映射后元素类型
+  * @param callback 映射函数，返回新元素
+  * @returns 新的 Deque 实例，包含映射后的元素
+  */
   public map<N>(callback: Callback<T, N>): Deque<N> {
     const deque = new Deque<N>();
 
@@ -189,15 +276,26 @@ export class Deque<T> {
     return deque;
   }
 
+  /**
+  * 清空队列，重置状态
+  */
   public clear() {
     this.head = this.tail = undefined;
     this.size = 0;
   }
 
+  /**
+  * 默认迭代器，支持 for...of 遍历
+  * @returns 迭代器，依次返回队列元素
+  */
   public [Symbol.iterator](): Iterator<T> {
     return this.values();
   }
 
+  /**
+  * 生成器函数，依次返回队列元素
+  * @private
+  */
   private *values(): Iterator<T> {
     let node = this.head;
 
