5fcb32c54dc56777d3be2396c315c3fa9ef0080d
NovaAstra <NovaAstra@outlook.com>
2025-06-03 22:52:04 +0800

    main

--- ui/headless/lozad/index.ts
+++ ui/headless/lozad/index.ts
@@ -2,6 +2,11 @@ export interface Options {
   intersectionObserverInit: IntersectionObserverInit;
 }
 
+export interface VisibilityMetric {
+  lastVisited?: number;
+  visitedCount?: number;
+}
+
 export class Queue {
 
 }
@@ -14,18 +19,41 @@ export class Scheduler {
 
 }
 
-export class Lozad<P, T> {
+export class Lozad<P, T extends VisibilityMetric> {
   public observer: IntersectionObserver;
 
   public readonly components: P[] = [];
 
   private initialized: boolean = false;
 
-  private mountedElements: WeakMap<HTMLElement, T> = new WeakMap();
+  private mountedElements: WeakMap<Element, T> = new WeakMap();
 
   public constructor(options: Partial<Options> = {}) { }
 
-  public observe(element: HTMLElement, data?: T) {
+  public launch() {
+    if (this.initialized) return;
+
+    this.observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      const now = Date.now();
+
+      for (const { target, isIntersecting } of entries) {
+        if (!this.mountedElements.has(target)) continue;
+
+        const element = this.mountedElements.get(target);
+        if (isIntersecting) {
+          element.lastVisited = now;
+          element.visitedCount = (element.visitedCount || 0) + 1
+          this.mountedElements.set(target, element);
+        }
+      }
+    });
+
+    this.initialized = true;
+  }
+
+  public observe(element: Element, data?: T) {
+    if (!this.initialized) this.launch();
+
     this.observer.observe(element);
     this.mountedElements.set(element, data);
   }
