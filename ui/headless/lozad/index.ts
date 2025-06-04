92145cc9d3273211a4248afef04d1292c9aaa085
August.Gao <August.Gao@mspbots.ai>
2025-06-04 19:02:28 +0800

    main

--- ui/headless/lozad/index.ts
+++ ui/headless/lozad/index.ts
@@ -2,24 +2,76 @@ export interface Options {
   intersectionObserverInit: IntersectionObserverInit;
 }
 
-export interface VisibilityMetric {
+export interface Metric {
   lastVisited?: number;
   visitedCount?: number;
+  status: Visibility;
+}
+
+export interface Component {
+
+}
+
+export interface Task {
+
+}
+
+export enum Visibility {
+  Hidden,
+  Visible
 }
 
 export class Queue {
+  public get size() {
+    return 0
+  }
 
+  public push() { }
+
+  public shift() { }
+
+  public clear() { }
 }
 
 export class Memory {
+  public constructor(public readonly lozad: Lozad<Component, Metric>) { }
+}
+
+export class Scroll {
 
 }
 
 export class Scheduler {
+  private running: boolean = false;
 
+  private idleCallbackId: number;
+
+  public launch() {
+
+  }
+
+  public schedule() {
+    if (this.idleCallbackId) cancelIdleCallback(this.idleCallbackId);
+
+    this.idleCallbackId = requestIdleCallback((deadline: IdleDeadline) => {
+      this.handle(deadline);
+    })
+  }
+
+  public terminate() {
+    if (this.idleCallbackId) {
+      cancelIdleCallback(this.idleCallbackId);
+      this.idleCallbackId = undefined;
+    }
+    this.running = false;
+  }
+
+  private handle(deadline: IdleDeadline) {
+    this.running = true;
+  }
 }
 
-export class Lozad<P, T extends VisibilityMetric> {
+export class Lozad<P extends Component, T extends Metric> extends Scheduler {
   public observer: IntersectionObserver;
 
   public readonly components: P[] = [];
@@ -28,9 +80,11 @@ export class Lozad<P, T extends VisibilityMetric> {
 
   private mountedElements: WeakMap<Element, T> = new WeakMap();
 
-  public constructor(options: Partial<Options> = {}) { }
+  public constructor(options: Partial<Options> = {}) {
+    super()
+  }
 
-  public launch() {
+  public bootstrap() {
     if (this.initialized) return;
 
     this.observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
@@ -39,23 +93,28 @@ export class Lozad<P, T extends VisibilityMetric> {
       for (const { target, isIntersecting } of entries) {
         if (!this.mountedElements.has(target)) continue;
 
-        const element = this.mountedElements.get(target);
+        const metric = this.mountedElements.get(target);
         if (isIntersecting) {
-          element.lastVisited = now;
-          element.visitedCount = (element.visitedCount || 0) + 1
-          this.mountedElements.set(target, element);
+          metric.lastVisited = now;
+          metric.visitedCount += 1;
+          metric.status = Visibility.Visible;
+          this.mountedElements.set(target, metric);
+          continue;
         }
+
+        metric.status = Visibility.Hidden;
+        this.mountedElements.set(target, metric);
       }
     });
 
     this.initialized = true;
   }
 
-  public observe(element: Element, data?: T) {
+  public observe(element: Element, data: T) {
     if (!this.initialized) this.launch();
 
     this.observer.observe(element);
-    this.mountedElements.set(element, data);
+    this.mountedElements.set(element, { ...data, visitedCount: 0 });
   }
 
   public dispose() {
