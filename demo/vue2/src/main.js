16cfb8a6c82ac238c0e18457186c31af5f249f8c
August.Gao <August.Gao@mspbots.ai>
2025-07-18 15:25:19 +0800

    初始化

--- demo/vue2/src/main.js
+++ demo/vue2/src/main.js
@@ -1,11 +1,14 @@
-
 import Vue from 'vue'
+import Element from 'element-ui'
 
 import App from '@/App.vue'
 
+Vue.use(Element, {
+  size: 'default',
+})
 
 Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
