37e7abae1a28b4582f0d93a5321aa86016430d2b
August.Gao <August.Gao@mspbots.ai>
2025-07-25 16:34:16 +0800

    main

---
+++ atom/charta/src/line/line.stories.ts
@@ -0,0 +1,13 @@
+import { ZenLine } from "./index"
+
+const meta = {
+  title: 'Charta/Line',
+  tags: ['autodocs'],
+  render: () => new ZenLine(),
+}
+
+export default meta;
+
+export const Primary = {
+
+};
