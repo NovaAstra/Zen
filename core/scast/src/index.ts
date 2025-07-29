faf781956fb678a42223d5e388cba3b50d4a80da
NovaAstra <NovaAstra@outlook.com>
2025-07-29 22:33:13 +0800

    main

--- core/scast/src/index.ts
+++ core/scast/src/index.ts
@@ -7,6 +7,8 @@ export interface Position {
 export interface Token {
   kind: TokenKind;
   value: string;
+  start: number;
+  next: Position;
 }
 
 export const enum TokenKind { }
@@ -103,14 +105,95 @@ export class Unicode {
   }
 }
 
-export class Tokenize {
-  public tokens = [];
+const is_ident = (c) => /[a-z0-9_-]/i.test(c)
 
-  public create(type, value, start) {
+const is_ident_start = (c) => /[a-z_]/i.test(c)
+
+const is_whitespace = (c) => '\t\r\n '.indexOf(c) >= 0
+
+export class Tokenize {
+  public readonly tokens: Token[] = [];
+
+  public constructor(public readonly reader: Reader) { }
+
+  public create(kind, value, start) {
     return Object.freeze({
-      type,
+      kind,
       value,
       start,
+      next: this.reader.position()
     })
   }
+
+  public peek(offset: number = 0) {
+    if (!this.tokens.length) {
+      const token = this.read_next()
+      if (token) this.tokens.push(token)
+    }
+    if (!offset) return this.tokens[0]
+    if (offset < this.tokens.length) return this.tokens[offset]
+    while (this.tokens.length <= offset) {
+      const token = this.read_next()
+      if (token) this.tokens.push(token)
+      else break
+    }
+    return this.tokens[offset]
+  }
+
+  public next() {
+    const token = this.tokens.shift()
+    return token || this.read_next()
+  }
+
+  public read_next() {
+    if (this.reader.eof()) return null;
+    const c = this.reader.peek();
+    if (is_whitespace(c)) {
+      return this.read_whitespace()
+    }
+    if (is_ident_start(c)) {
+      return this.read_ident()
+    }
+
+    if (c === '$') {
+      return this.read_variable()
+    }
+  }
+
+  public read_ident() {
+    const start = this.reader.position()
+    const value = this.read_while(is_ident)
+    return this.create('identifier', value, start)
+  }
+
+  public read_whitespace() {
+    const start = this.reader.position()
+    const value = this.read_while(is_whitespace)
+    return this.create('space', value, start)
+  }
+
+  public read_while(predicate) {
+    let s = ''
+    while (!this.reader.eof() && predicate(this.reader.peek())) {
+      s += this.reader.next()
+    }
+    return s
+  }
+
+  public read_variable() {
+    const start = this.reader.position()
+    this.reader.next()
+    const value = this.read_while(is_ident)
+    return this.create('variable', value, start)
+  }
+}
+
+export class Parse {
+  public constructor(tokens) { }
+
+  public stylesheet() {
+
+  }
+
+  public node() { }
 }
